57910b5a5e5d87bca5e5cfabc17e4c57fc717063
Aymeric <aymeric.legros@adixon.fr>
2018-11-02 15:53:18 +0100

    start steam auth

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Discuss.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:steamex, "~> 0.0.9"}
     ]
   end
 
--- web/router.ex
+++ web/router.ex
@@ -2,21 +2,30 @@ defmodule Discuss.Router do
   use Discuss.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
+  steamex_route_auth
+  
   scope "/", Discuss do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    # get "/", TopicController, :index
+    # get "/topics/new", TopicController, :new
+    # post "/topics", TopicController, :create
+    # get "/topics/:id/edit", TopicController, :edit
+    # put "/topics/:id", TopicController, :update
+
+    resources("/topics", TopicController)
   end
 
   # Other scopes may use custom stacks.
